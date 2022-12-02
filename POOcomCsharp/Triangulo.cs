7e85e76ae7896229767903599e1d246283ddffae
robbfreitas <robertocompromissos@gmail.com>
2022-12-02 11:37:35 -0300

    Finalizando projeto triângulo

--- POOcomCsharp/Triangulo.cs
+++ POOcomCsharp/Triangulo.cs
@@ -11,5 +11,11 @@ namespace POOcomCsharp
         public double A;
         public double B;
         public double C;
+
+        public double Area()
+        {
+            double p = (A + B + C) / 2.0;
+            return Math.Sqrt(p * (p - A) * (p - B) * (p - C));
+        }
     }
 }
